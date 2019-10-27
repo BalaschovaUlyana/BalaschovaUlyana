ddf579080c1302df55b12bc46c36e681c8fc4c4f
BalaschovaUlyana <56930389+BalaschovaUlyana@users.noreply.github.com>
2019-10-27 15:03:26 +0300

    Update Student.h

--- Лаборатория 01/Student.h
+++ Лаборатория 01/Student.h
@@ -1,29 +1,24 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-
+#pragma once
 #include<string>
 using namespace std;
 class Student
 {
-    public:
-        Student();
-        virtual ~Student();
-        void setFio(string secondName,string firstName, string patronymic);
-        string getFIO();
-        void setRating(int rating);
-        int getRating();
-        bool equalStudent(Student student);
-        void printStudent();
+public:
+	Student(const string& secondName, const string& firstName, const string& patronymic);
+	virtual ~Student();
+	string getFIO();
+	void setRating(int rating);
+	int getRating();
+	bool equalStudent(Student& student);
+	friend ostream& operator<<(ostream& stream, Student& obj);
 
 
-    protected:
+protected:
 
-    private:
-        string firstName;
-        string secondName;
-        string patronymic;
+private:
+	string firstName;
+	string secondName;
+	string patronymic;
 
-        int rating;
+	int rating;
 };
-
-#endif // STUDENT_H

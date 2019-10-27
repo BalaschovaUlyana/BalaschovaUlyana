f8b60a54d550a01af08bd13ecd2565a744121ae3
BalaschovaUlyana <56930389+BalaschovaUlyana@users.noreply.github.com>
2019-10-27 15:07:21 +0300

    Update StudentGroup.h

--- Лаборатория 01/StudentGroup.h
+++ Лаборатория 01/StudentGroup.h
@@ -1,3 +1,4 @@
+#pragma once
 #ifndef STUDENTGROUP_H
 #define STUDENTGROUP_H
 #include <list>
@@ -5,29 +6,22 @@
 #include "Student.h"
 class StudentGroup
 {
-    public:
-        StudentGroup();
-        virtual ~StudentGroup();
+public:
+	StudentGroup();
+	virtual ~StudentGroup();
+	void addStudent(Student& student);
+	void deleteStudent(Student& student);
+	Student* getStudentByFIO(string FIO);
+	list<Student> getSortedListByAlphabet();
+	list<Student> getSortedListByRating();
+	//дружественная функция перегрузки оператора вывода
+	friend ostream& operator<<(ostream& stream, StudentGroup& obj);
 
-        void addStudent(Student student);
-        void deleteStudent(Student student);
-        Student getStudentByFIO(string FIO);
-        list<Student> getSortedListByAlphabet();
-        list<Student> getSortedListByRating();
+protected:
 
-        //объявление дружеских функций компораторов для сравнения двух студентов
-        friend bool comAlphabet(Student& a, Student& b);
-        friend bool comRating(Student& a, Student& b);
-        //дружеская функция перегрузки оператора вывода
-        friend ostream &operator<<(ostream &stream, StudentGroup &obj);
-
-    protected:
-
-    private:
-        list<Student> students;
-        int maxStudents;
-       
-      
+private:
+	list<Student> students;
+	int maxStudents;
 };
 
 #endif // STUDENTGROUP_H
